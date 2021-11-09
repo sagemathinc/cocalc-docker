c1308c0b142c0b02a2e74ad4683df4e7e6030052
William Stein <wstein@sagemath.com>
2021-11-09 08:47:17 -0800

    tag "typo"

--- linux/update-the-build-stage-1.sh
+++ linux/update-the-build-stage-1.sh
@@ -3,5 +3,5 @@
 set -ev
 sudo docker stop cocalc-test
 sudo docker rm cocalc-test
-sudo docker push  sagemathinc/cocalc-aarch64:latest
-sudo docker push  sagemathinc/cocalc-aarch64:`cat current_commit`
+sudo docker push  sagemathinc/cocalc:latest
+sudo docker push  sagemathinc/cocalc:`cat current_commit`

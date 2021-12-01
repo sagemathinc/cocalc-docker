80140844ce797c56eb80a5f13546fa7a45944fcd
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2021-12-01 06:29:41 -0500

    keep up #/2

---
+++ linux/update-the-build-stage-1.sh
@@ -0,0 +1,6 @@
+ #!/usr/bin/env bash
+set -v
+sudo docker stop cocalc-test
+sudo docker rm cocalc-test
+sudo docker push  sagemathinc/cocalc:latest
+sudo docker push  sagemathinc/cocalc:`cat current_commit`
---
+++ m1/update-the-build-stage-1.sh
@@ -0,0 +1,7 @@
+
+
+set -v
+docker stop cocalc-test
+docker rm cocalc-test
+docker push  sagemathinc/cocalc-aarch64:latest
+docker push  sagemathinc/cocalc-aarch64:`cat current_commit` 

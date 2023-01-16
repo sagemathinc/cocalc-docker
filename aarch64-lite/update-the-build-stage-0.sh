e799ad71c2e1cd9299228071365ee0c8082360bd
William Stein <wstein@sagemath.com>
2023-01-16 14:50:36 -0800

    fix build of cocalc docker personal (by installing pnpm)

--- aarch64-lite/update-the-build-stage-0.sh
+++ aarch64-lite/update-the-build-stage-0.sh
@@ -7,7 +7,7 @@ export BRANCH="${BRANCH:-master}"
 echo "BRANCH=$BRANCH"
 commit=`git ls-remote -h https://github.com/sagemathinc/cocalc $BRANCH | awk '{print $1}'`
 echo $commit | cut -c-12 > current_commit
-time docker build --build-arg commit=$commit --build-arg BRANCH=$BRANCH --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -t cocalc-lite-aarch64 $@ ..
+time docker build --build-arg commit=$commit --build-arg BRANCH=$BRANCH --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')  -f ../Dockerfile-lite -t cocalc-lite-aarch64 $@ ..
 docker tag cocalc-lite-aarch64:latest sagemathinc/cocalc-lite-aarch64:latest
 docker tag cocalc-lite-aarch64:latest sagemathinc/cocalc-lite-aarch64:`cat current_commit`
 docker run --name=cocalc-lite-test -d -p 127.0.0.1:4043:443 sagemathinc/cocalc-lite-aarch64

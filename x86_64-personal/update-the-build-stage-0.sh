dd86c2925c1a18f41be2c3c3c75a43b51a31f86e
William Stein <wstein@sagemath.com>
2023-01-19 16:05:16 +0000

    make test container for personal be totally ephemeral (otherwise hit permissions issues)

--- x86_64-personal/update-the-build-stage-0.sh
+++ x86_64-personal/update-the-build-stage-0.sh
@@ -9,4 +9,4 @@ echo $commit | cut -c-12 > current_commit
 time docker build --build-arg commit=$commit --build-arg BRANCH=$BRANCH --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -f ../Dockerfile-personal -t cocalc-personal $@ ..
 docker tag cocalc-personal:latest sagemathinc/cocalc-personal:latest
 docker tag cocalc-personal:latest sagemathinc/cocalc-personal:`cat current_commit`
-docker run --name=cocalc-test-personal -d -v  ~/cocalc-test-personal:/home/user/cocalc/src/data -p 127.0.0.1:5123:5000 sagemathinc/cocalc-personal
+docker run --name=cocalc-test-personal -d -p 127.0.0.1:5123:5000 sagemathinc/cocalc-personal

b0f76d0208c2c977b7be232a6f99b63c51688421
William Stein <wstein@sagemath.com>
2023-08-23 17:14:57 -0700

    sage-10.1

--- aarch64-lite/update-the-build-stage-0.sh
+++ aarch64-lite/update-the-build-stage-0.sh
@@ -11,4 +11,4 @@ echo $commit | cut -c-12 > current_commit
 time docker build --build-arg commit=$commit --build-arg BRANCH=$BRANCH --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')  -f ../Dockerfile-lite -t cocalc-lite-aarch64 $@ ..
 docker tag cocalc-lite-aarch64:latest sagemathinc/cocalc-v2-lite-aarch64:latest
 docker tag cocalc-lite-aarch64:latest sagemathinc/cocalc-v2-lite-aarch64:`cat current_commit`
-docker run --name=cocalc-lite-test -d -p 127.0.0.1:4043:443 sagemathinc/cocalc-v2-lite-aarch64
+docker run --name=cocalc-lite-test -d -p 127.0.0.1:4044:443 sagemathinc/cocalc-v2-lite-aarch64
